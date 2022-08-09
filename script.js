e9498dbb0d3c5ee2ff31041b1f15288b2c2dd399
Lucas <60785299+Lucaas27@users.noreply.github.com>
2022-08-09 16:47:25 +0100

    Resize slider

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const container = document.querySelector(".container");
-const cells = document.querySelectorAll(".grid-item");
 const root = document.documentElement;
 const slider = document.getElementById("myRange");
-const gridSize = document.getElementById("grid__value");
-let pickedBgColor = 'rgb(0,0,0)'
+const gridSize = document.getElementById("slider__value");
+let pickedBgColor = "rgb(0,0,0)";
 
 /* 
 Replace the grid-row/grid-col properties from root
@@ -13,7 +12,7 @@ it will default to 16x16
 function makeGrid(row = 16, col = 16) {
   root.style.setProperty("--grid-row", row);
   root.style.setProperty("--grid-col", col);
-  container.setAttribute('draggable', false);
+  container.setAttribute("draggable", false);
   // iterate over the area of the container and creates cells to append to it
   for (let i = 0; i < row * col; i++) {
     const cell = document.createElement("div");
@@ -24,19 +23,23 @@ function makeGrid(row = 16, col = 16) {
 
 function paint() {
   //Listens for two events and calls appropriate function
-  ['mousedown', 'mouseup'].forEach(evt => {
+  ["mousedown", "mouseup"].forEach((evt) => {
     //Assign a function to callback, depending on the event.
     let callback;
     switch (evt) {
-      case 'mousedown':
+      case "mousedown":
         callback = mouseDownListener;
         break;
-      case 'mouseup':
+      case "mouseup":
         callback = mouseUpListener;
         break;
+      default:
+        console.log(
+          `Error.The callback function for event listeners is not working.`
+        );
     }
     container.addEventListener(evt, callback);
-  })
+  });
 }
 
 // Change the background color
@@ -49,25 +52,23 @@ will trigger another event listener - mousemove.*/
 function mouseDownListener(e) {
   changeColor(e);
   // console.log(e.target);
-  container.addEventListener('mousemove', changeColor);
+  container.addEventListener("mousemove", changeColor);
 }
 
-// Remove mousemove listener 
+// Remove mousemove listener
 function mouseUpListener() {
-  container.removeEventListener('mousemove', changeColor);
+  container.removeEventListener("mousemove", changeColor);
 }
-
 
 function changeGrid() {
   gridSize.textContent = `${slider.value}x${slider.value}`;
 
-  slider.oninput = function () {
+  slider.oninput = () => {
     gridSize.textContent = `${this.value}x${this.value}`;
-  }
+  };
 }
 window.onload = () => {
   makeGrid();
   paint();
   changeGrid();
 };
-
